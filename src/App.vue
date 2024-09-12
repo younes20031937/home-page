61c2de9f5db6182d820b675abbe40cc12492429d
younes <younesboukdir100@gmail.com>
2024-09-12 23:17:18 +0100

    push

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Card from './Card.vue';
+</script>
+<template>
+ <Card title="First" icon="home" modal="firstModal" />
+ <Card title="Second" icon="user" modal="secondModal"/>
+ <Card title="Third" icon="cog" modal="thirdModal"/>
+</template>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import './assets/main.css'
+import 'primeicons/primeicons.css' // Import PrimeIcons CSS
+
+createApp(App).mount('#app')
